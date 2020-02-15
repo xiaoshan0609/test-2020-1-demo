d6d7d47c78018e08364301503c17d80062dd27d0
xiaoshan0609 <xiaoshan0609@126.com>
2020-02-15 21:12:52 +0800

    Version25.0

--- resnet_utils.py
+++ resnet_utils.py
@@ -4,11 +4,12 @@ from keras.layers import Dense, Dropout,Flatten,Conv1D,MaxPooling1D,BatchNormali
 from keras.layers.core import Activation
 from keras.models import Model, Input
 
-def Unit(x,filters,pool=False):
+def Unit(x,filters,pool=False)
     res = x
     if pool:
         x = MaxPooling1D(pool_size=(2))(x)
         res = Conv1D(filters=filters,kernel_size=[1],strides=(2),padding="same")(res)
+        res =BatchNormalization()(res)
     out = BatchNormalization()(x)
     out = Activation("relu")(out)
     out = Conv1D(filters=filters, kernel_size=[3], strides=[1], padding="same")(out)
@@ -23,26 +24,23 @@ def Unit(x,filters,pool=False):
     
 def MiniModel(input_shape):
     data = Input(input_shape)
-    net = Conv1D(filters=32, kernel_size=[3], strides=[1], padding="same")(data)
+    net = Conv1D(filters=64, kernel_size=[3], strides=[1], padding="same")(data)
+    net = Unit(net,64,pool=True)
+   
+    net = Unit(net,32,pool=True)
     net = Unit(net,32)
    
- 
-    net = Unit(net,64,pool=True)
-    net = Unit(net,64)
- 
- 
-    net = Unit(net,64,pool=True)
-    net = Unit(net,64)
-
-   
- 
+    net = Unit(net,32,pool=True)
+    net = Unit(net,32)
+    
     net = BatchNormalization()(net)
     net = Activation("relu")(net)
     net = Dropout(0.25)(net)
  
-    net = AveragePooling1D(pool_size=(4))(net)
+    net = MaxPooling1D(pool_size=(4))(net)
     net = Flatten()(net)
-    net = Dense(units=32, activation='sigmoid')(net) 
+    net = Dense(units=128, activation='sigmoid')(net)
+    net = Dense(units=32, activation='sigmoid')(net)     
     net = Dense(units=3,activation="softmax")(net)
     model = Model(inputs=data,outputs=net)
  
